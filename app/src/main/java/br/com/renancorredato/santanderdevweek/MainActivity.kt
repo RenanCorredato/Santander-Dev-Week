28cbfc1e7489bef8571d8de505943495f692e8d3
Renan Cleber <renancorredato@hotmail.com>
2021-06-02 11:09:10 -0300

    Tela santander

---
+++ app/src/main/java/br/com/renancorredato/santanderdevweek/MainActivity.kt
@@ -0,0 +1,32 @@
+package br.com.renancorredato.santanderdevweek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // chamdo a main principal
+        setSupportActionBar(findViewById(R.id.toolbar)) // chamando pelo id o menu
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean { // sobre escrendo o metodo do meu e infaldno ( criando corpo)
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // escutando item do menu
+        return  when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("CLICK","Click no item 1 ")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
